28e6ea1f431c7b236cdb141c42ddbfa202da8224
Trung Le <lhtrungst@gmail.com>
2017-05-24 00:12:35 +0700

    add 2017 child theme + update site-info

---
+++ wp-content/themes/twentyseventeenchild/template-parts/footer/site-info.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Displays footer site info
+ *
+ * @package WordPress
+ * @subpackage Twenty_Seventeen
+ * @since 1.0
+ * @version 1.0
+ */
+
+?>
+<div class="site-info">
+	<!--<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'twentyseventeen' ) ); ?>"><?php printf( __( 'Proudly powered by %s', 'twentyseventeen' ), 'WordPress' ); ?></a>-->
+	Copyright &#169;  <?php echo date("Y"); ?> by <a href="<?php echo esc_url( __( 'http://congtyhongduc.com/', 'twentyseventeen' ) ); ?>"> congtyhongduc.com. All rights reserved.
+	<!--/*<a href="<?php echo esc_url( __( 'http://congtyhongduc.com/', 'twentyseventeen' ) ); ?>"><?php printf( __( '@Copyright %s', 'twentyseventeen' ), 'congtyhongduc.com' ); ?></a>*/-->
+</div><!-- .site-info -->

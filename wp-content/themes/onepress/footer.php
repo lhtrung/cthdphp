c6a8bfcc7642bf7bf916d35bdc919f251114ef0a
Trung Le <lhtrungst@gmail.com>
2017-07-23 13:20:19 +0700

    custom footer + adjust color css

--- wp-content/themes/onepress/footer.php
+++ wp-content/themes/onepress/footer.php
@@ -9,83 +9,34 @@
  * @package OnePress
  */
 ?>
-    <footer id="colophon" class="site-footer" role="contentinfo">
-        <?php
-        $onepress_btt_disable = get_theme_mod('onepress_btt_disable');
-        $onepress_social_footer_title = get_theme_mod('onepress_social_footer_title', esc_html__('Keep Updated', 'onepress'));
-
-        $onepress_newsletter_disable = get_theme_mod('onepress_newsletter_disable', '1');
-        $onepress_social_disable = get_theme_mod('onepress_social_disable', '1');
-        $onepress_newsletter_title = get_theme_mod('onepress_newsletter_title', esc_html__('Join our Newsletter', 'onepress'));
-        $onepress_newsletter_mailchimp = get_theme_mod('onepress_newsletter_mailchimp');
-
-        if ( $onepress_newsletter_disable != '1' || $onepress_social_disable != '1' ) : ?>
-            <div class="footer-connect">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-sm-2"></div>
-                        <?php if ($onepress_newsletter_disable != '1') : ?>
-                            <div class="col-sm-4">
-                                <div class="footer-subscribe">
-                                    <?php if ($onepress_newsletter_title != '') echo '<h5 class="follow-heading">' . $onepress_newsletter_title . '</h5>'; ?>
-                                    <form novalidate="" target="_blank" class="" name="mc-embedded-subscribe-form" id="mc-embedded-subscribe-form" method="post"
-                                          action="<?php if ($onepress_newsletter_mailchimp != '') echo $onepress_newsletter_mailchimp; ?>">
-                                        <input type="text" placeholder="<?php esc_attr_e('Enter your e-mail address', 'onepress'); ?>" id="mce-EMAIL" class="subs_input" name="EMAIL" value="">
-                                        <input type="submit" class="subs-button" value="<?php esc_attr_e('Subscribe', 'onepress'); ?>" name="subscribe">
-                                    </form>
-                                </div>
-                            </div>
-                        <?php endif; ?>
-
-                        <div class="<?php if ( $onepress_newsletter_disable == '1' ) {
-                            echo 'col-sm-8';
-                        } else {
-                            echo 'col-sm-4';
-                        } ?>">
-                            <?php
-                            if ($onepress_social_disable != '1') {
-                                ?>
-                                <div class="footer-social">
-                                    <?php
-                                    if ($onepress_social_footer_title != '') echo '<h5 class="follow-heading">' . $onepress_social_footer_title . '</h5>';
-
-                                    $socials = onepress_get_social_profiles();
-                                    /**
-                                     * New Socials profiles
-                                     *
-                                     * @since 1.1.4
-                                     * @change 1.2.1
-                                     */
-                                    echo '<div class="footer-social-icons">';
-                                    if ( $socials ) {
-                                        echo $socials;
-                                    } else {
-                                        /**
-                                         * Deprecated
-                                         * @since 1.1.4
-                                         */
-                                        $twitter = get_theme_mod('onepress_social_twitter');
-                                        $facebook = get_theme_mod('onepress_social_facebook');
-                                        $google = get_theme_mod('onepress_social_google');
-                                        $instagram = get_theme_mod('onepress_social_instagram');
-                                        $rss = get_theme_mod('onepress_social_rss');
-
-                                        if ($twitter != '') echo '<a target="_blank" href="' . $twitter . '" title="Twitter"><i class="fa fa-twitter"></i></a>';
-                                        if ($facebook != '') echo '<a target="_blank" href="' . $facebook . '" title="Facebook"><i class="fa fa-facebook"></i></a>';
-                                        if ($google != '') echo '<a target="_blank" href="' . $google . '" title="Google Plus"><i class="fa fa-google-plus"></i></a>';
-                                        if ($instagram != '') echo '<a target="_blank" href="' . $instagram . '" title="Instagram"><i class="fa fa-instagram"></i></a>';
-                                        if ($rss != '') echo '<a target="_blank" href="' . $rss . '"><i class="fa fa-rss"></i></a>';
-                                    }
-                                    echo '</div>';
-                                    ?>
-                                </div>
-                            <?php } ?>
-                        </div>
-                        <div class="col-sm-2"></div>
-                    </div>
-                </div>
+    <footer id="colophon" class="site-footer" role="contentinfo">   
+          <!--Trung Starts-Add Footer Sidebar -->
+        <div id="footer-sidebar" class="secondary">
+            <div id="footer-sidebar1">
+                <?php
+                if(is_active_sidebar('footer-sidebar-1')){
+                    dynamic_sidebar('footer-sidebar-1');
+                }
+                ?>
             </div>
-        <?php endif; ?>
+            <div id="footer-sidebar2">
+                <?php
+                if(is_active_sidebar('footer-sidebar-2')){
+                    dynamic_sidebar('footer-sidebar-2');
+                }
+                ?>
+            </div>
+            <div id="footer-sidebar3">
+                <?php
+                if(is_active_sidebar('footer-sidebar-3')){
+                    dynamic_sidebar('footer-sidebar-3');
+                }
+                ?>
+            </div>
+        </div>
+        <!--Trung Ends-Add Footer Sidebar -->
+
+        
 
         <div class="site-info">
             <div class="container">
